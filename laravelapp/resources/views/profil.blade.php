6d41c58e3bc6a9230f2a313dc4a3f864e18a65cd
DinaLisuardi <lisuardidina@gmail.com>
2020-04-06 10:26:50 +0700

    PWL Laravel

---
+++ laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Profil Saya</title>
+</head>
+<body>
+ <h1>Profil Saya</h1>
+ <p>Perkenalkan Nama saya Dina Lisuardi, saya seorang Mahasiswi di Politeknik Negeri Malang
+.</p>
+</body>
+</html>
